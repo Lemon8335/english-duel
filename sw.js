1ec80205b513aeb50e672bd70eba3e903feb7ec4
Lemon8335 <tinakmatej@gmail.com>
2026-05-03 10:12:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE = 'english-duel-v1';
+const CACHE = 'english-duel-v2';
 const ASSETS = [
   '/english-duel/',
-  '/english-duel/index.html'
+  '/english-duel/index.html',
+  '/english-duel/manifest.json',
+  '/english-duel/icon-192.png',
+  '/english-duel/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => Promise.all(ASSETS.map(asset => fetch(asset).then(r => { if (r.ok) return c.put(asset, r); }).catch(() => null))))
+      .then(() => self.skipWaiting())
   );
 });
 
